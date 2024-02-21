0127cba59160e3aa6af9b55e067cd218812bf093
Nerea González Selva <47923571+nereagonzalez12@users.noreply.github.com>
2024-02-21 14:00:04 +0100

    funciones http

--- src/app/quienes-somos/quienes-somos.component.ts
+++ src/app/quienes-somos/quienes-somos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Placeholder } from '../interfaces/placeholder';
 
 @Component({
   selector: 'app-quienes-somos',
@@ -21,10 +22,65 @@ export class QuienesSomosComponent implements OnInit {
   }
 
   get2() {
-    this.http.get(this.url + '/' + this.inputIdGet)
+    this.http.get<Placeholder>(this.url + '/' + this.inputIdGet)
       .subscribe(data => {
         this.resultadoPeticion = data;
-        this.resultadoPeticion.userId;
+        console.log(this.resultadoPeticion.body);
+        console.log(this.resultadoPeticion.title);
+        console.log(this.resultadoPeticion.id);
+        console.log(this.resultadoPeticion.userId);
       });
   }
+
+  get3() {
+    this.http.get<Placeholder>(this.url + '/' + this.inputIdGet)
+      .subscribe(
+        {
+          next: (respuesta: Placeholder) => {
+            this.resultadoPeticion = respuesta;
+            console.log(this.resultadoPeticion.body);
+            console.log(this.resultadoPeticion.title);
+            console.log(this.resultadoPeticion.id);
+            console.log(this.resultadoPeticion.userId);
+          },
+          error: (err) => {
+            console.log('error');
+
+          }
+        });
+  }
+
+  post() {
+    this.http.post(this.url,
+      {
+        userId: 2,
+        id: 209,
+        title: 'nuevo titulo',
+        body: 'nuevo cuerpo'
+      }).subscribe(data => { this.resultadoPeticion = data; });
+  }
+
+  put() {
+    this.http.put(this.url + '/10',
+      {
+        userId: 2,
+        title: 'nuevo titulo',
+        body: 'nuevo cuerpo'
+      }).subscribe(data => { this.resultadoPeticion = data; });
+  }
+
+  patch() {
+    this.http.patch(this.url + '/10',
+      {
+        userId: 2,
+        title: 'nuevo titulo',
+        body: 'nuevo cuerpo'
+      }).subscribe(data => { this.resultadoPeticion = data; });
+  }
+
+  delete() {
+    this.http.delete(this.url + '/10')
+      .subscribe(data => { this.resultadoPeticion = data; });
+  }
 }
+
